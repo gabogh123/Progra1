71a80569dfe8381e80d030411abe718bdf815615
gabrielgh123 <gabogh99@hotmail.com>
2018-09-11 00:58:46 -0600

    Creación de la clase Dot

--- Proyecto 1/src/proyecto/VentanaInicio.java
+++ Proyecto 1/src/proyecto/VentanaInicio.java
@@ -1,5 +1,56 @@
 package proyecto;
 
-public class VentanaInicio {
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-}
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+
+
+public class VentanaInicio extends JFrame implements ActionListener{
+
+		
+		JLabel texto;
+		JButton boton1;
+		
+		public VentanaInicio(){
+			
+			super("Ventana");
+			setBounds(200,200,500,500);
+			setVisible(true);
+			setResizable(false);
+			setLayout(new FlowLayout());
+			this.getContentPane().setBackground(Color.cyan);
+			
+			boton1 = new JButton("Boton 1");
+
+			boton1.addActionListener(this);
+
+			
+			add(boton1);
+
+		}
+		
+		@Override
+		public void actionPerformed(ActionEvent evento) {
+			
+			if(evento.getSource() == boton1){
+				
+				
+				
+			}
+	
+		}
+		
+		public static void main(String[] args){
+			VentanaInicio ventana = new VentanaInicio();
+			
+		
+		}
+	
+	}
+
